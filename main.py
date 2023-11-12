f652b7fa1095f431fbe435ba15b4182a0759dcfc
Kobzar DV <kdmitriyv@bk.ru>
2023-11-12 22:01:22 +0300

    added paddingTest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import PIL
+import sys
 import numpy as np
 from PIL import Image
 
@@ -6,14 +7,35 @@ def pilToNumpy(img):
     return np.array(img)
 
 def numpyToPil(img):
-    return Image.fromarray(img)
+    output = Image.fromarray(img)
+    if output.mode != 'RGB':
+        output = output.convert('RGB')
+    return output
 
 def padding(img):
-    paddingSize
+    paddingSize = 2
+    maxImgSize = max(img.shape)
+    while maxImgSize > paddingSize:
+        paddingSize *= 2
 
+    paddingImg = np.zeros([paddingSize,paddingSize,3],dtype=np.uint8)
+    xShift = (paddingSize - img.shape[0]) // 2
+    yShift = (paddingSize - img.shape[1]) // 2
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            paddingImg[xShift+i][yShift+j] = img[i][j]
+    return paddingImg
 
+def testPadding():
+    np.set_printoptions(threshold=sys.maxsize)
 
-if __name__ == '__main__':
-    img = PIL.Image.open("temp.png").convert("L")
+    img = PIL.Image.open("500x500.png").convert("RGB")
     imgarr = pilToNumpy(img)
     print(imgarr.shape)
+
+    paddingImg = padding(imgarr)
+    pilImg = numpyToPil(paddingImg)
+    pilImg.save("paddingOutput.jpg")
+
+if __name__ == '__main__':
+    testPadding()
